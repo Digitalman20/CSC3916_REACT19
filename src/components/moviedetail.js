bea384005ac6b6bdefdc39613e7cd071559c2dee
Digitalman20 <130844003+Digitalman20@users.noreply.github.com>
2026-04-16 11:02:07 -0600

    env retrieve fix

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -45,7 +45,7 @@ const MovieDetail = () => {
     const token = localStorage.getItem("token");
 
     try {
-      const res = await fetch(`${import.meta.env.REACT_APP_API_URL}/reviews`, {
+      const res = await fetch(process.env.REACT_APP_API_URL + "/reviews", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
